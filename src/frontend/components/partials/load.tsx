54907e2fa6f355ef92c7d59205e887648aa20a40
CrackNicolas <beltrannicolasalejo@gmail.com>
2025-01-23 01:26:36 -0300

    refactor/applying-good-practices

--- src/frontend/components/partials/load.tsx
+++ src/frontend/components/partials/load.tsx
@@ -3,7 +3,7 @@ import { Component } from "@/frontend/types/component";
 export default function ComponentLoad(): Component {
     return (
         <div className="absolute inset-0 flex flex-col gap-y-5 justify-center items-center py-[110px] ">
-            <div className="animate-spin rounded-full size-[70px] border-t-[1.5px] border-b-[1.5px] dark:border-dark-secondary border-secondary" />
+            <div className="animate-spin rounded-full w-[70px] h-[70px] border-t-[1.5px] border-b-[1.5px] dark:border-dark-secondary border-secondary" />
             <span className="dark:text-dark-secondary text-secondary tracking-wider">
                 Cargando
             </span>
