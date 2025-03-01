c0584f4e68f39606db3486aef5bef667ba61a248
CrackNicolas <beltrannicolasalejo@gmail.com>
2025-03-01 20:54:02 -0300

    refactor/applying-good-practices

--- src/frontend/components/layouts/home/sections/main/container.tsx
+++ src/frontend/components/layouts/home/sections/main/container.tsx
@@ -10,21 +10,24 @@ export default function ComponentMainHome(): Component {
     const { translate } = useAppTranslation();
 
     return (
-        <ComponentMotion type="section" descriptionClass="mt-16 grid grid-cols-1 md:grid-cols-3 gap-7 md:gap-6">
+        <ComponentMotion type="section" descriptionClass="mt-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7 md:gap-6">
             <ComponentItemMainHome
-                icon={<FaRegLightbulb className="text-secondary dark:text-seventh text-opacity-60 text-4xl sm:text-5xl pb-2 md:mb-4" />}
-                title={translate('home.sections.main.article_1.title')}
-                subtitle={translate('home.sections.main.article_1.subtitle')}
-            />
-            <ComponentItemMainHome
+                descriptionClass="col-span-1"
                 icon={<FaRocket className="text-secondary dark:text-seventh text-opacity-60 text-4xl sm:text-5xl pb-2 md:mb-4" />}
                 title={translate('home.sections.main.article_2.title')}
                 subtitle={translate('home.sections.main.article_2.subtitle')}
             />
             <ComponentItemMainHome
+                descriptionClass="col-span-1"
                 icon={<FaCogs className="text-secondary dark:text-seventh text-opacity-60 text-4xl sm:text-5xl pb-2 md:mb-4" />}
                 title={translate('home.sections.main.article_3.title')}
                 subtitle={translate('home.sections.main.article_3.subtitle')}
+            />
+            <ComponentItemMainHome
+                descriptionClass="sm:col-span-2 lg:col-span-1"
+                icon={<FaRegLightbulb className="text-secondary dark:text-seventh text-opacity-60 text-4xl sm:text-5xl pb-2 md:mb-4" />}
+                title={translate('home.sections.main.article_1.title')}
+                subtitle={translate('home.sections.main.article_1.subtitle')}
             />
         </ComponentMotion>
     )
--- src/frontend/components/layouts/home/sections/main/item.tsx
+++ src/frontend/components/layouts/home/sections/main/item.tsx
@@ -3,15 +3,15 @@ import { Component } from "@/frontend/types/component";
 import IElement from "@/frontend/interfaces/elements/element";
 import ComponentMotion from "@/frontend/components/partials/motion";
 
-interface IItemMainHome extends Pick<IElement, 'title' | 'subtitle'> {
+interface IItemMainHome extends Pick<IElement, 'title' | 'subtitle' | 'descriptionClass'> {
     icon: Component
 }
 
 export default function ComponentItemMainHome(props: IItemMainHome): Component {
-    const { icon, title, subtitle } = props;
+    const { icon, title, subtitle, descriptionClass } = props;
 
     return (
-        <ComponentMotion type="div" descriptionClass="flex flex-col items-center py-3 pl-3 pr-6 md:py-5 md:pl-5 md:pr-8 rounded-lg bg-gradient-to-r from-primary dark:from-tertiary dark:from-[99%] from-95% to-secondary dark:to-seventh dark:to-[1%] to-2% dark:shadow-xl dark:border-[1px] shadow-sm shadow-secondary">
+        <ComponentMotion type="div" descriptionClass={`${descriptionClass} col flex flex-col items-center py-3 pl-3 pr-6 md:py-5 md:pl-5 md:pr-8 rounded-lg bg-gradient-to-r from-primary dark:from-tertiary dark:from-[99%] from-95% to-secondary dark:to-seventh dark:to-[1%] to-2% dark:shadow-xl dark:border-[1px] shadow-sm shadow-secondary`}>
             {icon}
             <h3 className="text-lg text-center sm:text-xl font-semibold md:mb-2 text-gradient">
                 {title}
